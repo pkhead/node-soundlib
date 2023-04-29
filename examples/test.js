6050630957fd3d6527edcaf4c9ddb4ab1d702039
pkhead <chromosoze@gmail.com>
2023-04-29 15:50:35 -0500

    update index.js

--- examples/test.js
+++ examples/test.js
@@ -1,10 +1,15 @@
 const audiolib = require("../index");
 
-let outputDevice = new audiolib.AudioOutputDevice();
+let outputDevice = new audiolib.AudioOutputDevice({
+    float: true,
+    bitDepth: 32
+});
+
 let FREQ = 440;
 let time = 0;
 
 const BUF_SIZE = 512;
+const NUM_RANGE = 1;
 let buf = new Float32Array(BUF_SIZE * outputDevice.channelCount);
 
 process.stdin.on("data", (chunk) => {
@@ -19,14 +24,14 @@ process.stdin.on("data", (chunk) => {
 
 setInterval(() => {
     while (outputDevice.numQueuedFrames < BUF_SIZE * 5) {
+        let max = -Infinity; 
         for (let i = 0; i < buf.length; i += 2) {
             let val = 0.1 * Math.sin(time);
-            buf[i] = val;
-            buf[i + 1] = val;
-
+            buf[i] = val * NUM_RANGE;
+            buf[i + 1] = val * NUM_RANGE;
             time += (FREQ * 2 * Math.PI) / outputDevice.sampleRate;
         }
         
         outputDevice.queue(buf.buffer);
     }
-}, 5);
+}, 10);
